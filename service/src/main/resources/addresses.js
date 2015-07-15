7c56036a025f25004160bcc1db5c0e659a6de7fc
Martins Rumkovskis <martins@uniso.lv>
2015-07-16 01:11:48 +0300

    refactoring

--- service/src/main/resources/addresses.js
+++ service/src/main/resources/addresses.js
@@ -3,10 +3,6 @@ var addresses = angular.module('addresses', []);
 addresses.controller('AddressesCtrl', function($scope, $http, $interval) {
   $scope.search = '';
   $scope.addresses = [];
-  $http.get('version').success(function(data) {
-  	var i = data.indexOf('.');
-  	$scope.version = i == -1 ? data : data.substring(0, i);
-  });
   $scope.updateAddress = function () {
     var searched = $scope.search;
     $http.get('address', {params: {search: $scope.search}}).success(function(data) {
@@ -14,5 +10,12 @@ addresses.controller('AddressesCtrl', function($scope, $http, $interval) {
         $scope.addresses = data;
       }
     });
-  }
+  };
+  $scope.updateVersion = function() {
+    $http.get('version').success(function(data) {
+      var i = data.indexOf('.');
+      $scope.version = i == -1 ? data : data.substring(0, i);
+    });    
+  };
+  $scope.updateVersion();
 });
