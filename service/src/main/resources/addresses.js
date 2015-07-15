41f6576a7319ed2ab99872f8fdc8363bce420c5a
Martins Rumkovskis <martins@uniso.lv>
2015-07-15 18:49:35 +0300

    test page

--- service/src/main/resources/addresses.js
+++ service/src/main/resources/addresses.js
@@ -1,8 +1,23 @@
 var addresses = angular.module('addresses', []);
 
-addresses.controller('AddressesCtrl', function($scope, $http) {
+addresses.controller('AddressesCtrl', function($scope, $http, $interval) {
+  $scope.search = '';
+  $scope.addresses = [];
   $http.get('version').success(function(data) {
   	var i = data.indexOf('.');
   	$scope.version = i == -1 ? data : data.substring(0, i);
   });
+  $scope.currentSearch = '';
+  $interval(function () {
+    var localCurrentSearch = '';
+    if ($scope.search !== $scope.currentSearch) {
+      localCurrentSearch = $scope.search;
+      $http.get('address', {params: {search: $scope.search}}).success(function(data) {
+        if ($scope.search === localCurrentSearch) {
+          $scope.addresses = data;
+          $scope.currentSearch = localCurrentSearch;
+        }
+      });
+    }
+  }, 200);
 });
