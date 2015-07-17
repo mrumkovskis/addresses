968c303120627e839e1ec287546501121a6b7820
Martins Rumkovskis <martins@uniso.lv>
2015-07-17 18:51:35 +0300

    refactoring, limit support wip

--- service/src/main/resources/addresses.js
+++ service/src/main/resources/addresses.js
@@ -5,30 +5,33 @@ addresses.controller('AddressesCtrl', function($scope, $http, $filter,$httpParam
   $scope.addresses = [];
 
   $scope.types = [
-    { name: '104', selected: false, nosaukums: 'pilsēta' },
-    { name: '113', selected: false, nosaukums: 'novads' },
-    { name: '105', selected: false, nosaukums: 'pagasts' },
-    { name: '106', selected: false, nosaukums: 'ciems' },
-    { name: '107', selected: false, nosaukums: 'iela' },
-    { name: '108', selected: false, nosaukums: 'māja' },
-    { name: '109', selected: false, nosaukums: 'dzīvoklis' }];
+    {value: '104', selected: false, name: 'pilsēta'},
+    {value: '113', selected: false, name: 'novads'},
+    {value: '105', selected: false, name: 'pagasts'},
+    {value: '106', selected: false, name: 'ciems'},
+    {value: '107', selected: false, name: 'iela'},
+    {value: '108', selected: false, name: 'māja'},
+    {value: '109', selected: false, name: 'dzīvoklis'}];
+
+  $scope.limit = {value: 20, selected: false, name: 'limits'};
 
   $scope.calculateUrlParams = function() {
-    $scope.selectedTypes = [];
+    var types = [];
     angular.forEach($filter('filter')($scope.types, {selected: true}), function(value, key) {
-      this.push(value.name);
-    },$scope.selectedTypes);
-    $scope.urlParams = $httpParamSerializer({search: $scope.search, type: $scope.selectedTypes});
+      this.push(value.value);
+    }, types);
+    var limit = [];
+    if ($scope.limit.selected) { limit.push($scope.limit.value); };
+    return {search: $scope.search, type: types, limit: limit};
   };
 
   $scope.updateAddress = function () {
     var searched = $scope.search;
-    if ($scope.search.length > 0) {
-      $scope.calculateUrlParams();
-    }
-    $http.get('address', {params: {search: $scope.search, type: $scope.selectedTypes}}).success(function(data) {
+    var params = $scope.calculateUrlParams();
+    $http.get('address', {params: params}).success(function(data) {
       if ($scope.search === searched) {
         $scope.addresses = data;
+        $scope.urlParams = $httpParamSerializer(params);
       }
     });
   };
@@ -37,12 +40,8 @@ addresses.controller('AddressesCtrl', function($scope, $http, $filter,$httpParam
     a.extended_view = ! a.extended_view;
   }
 
-  $scope.updateVersion = function() {
-    $http.get('version').success(function(data) {
-      var i = data.indexOf('.');
-      $scope.version = i == -1 ? data : data.substring(0, i);
-    });
-  };
-
-  $scope.updateVersion();
+  $http.get('version').success(function(data) {
+    var i = data.indexOf('.');
+    $scope.version = i == -1 ? data : data.substring(0, i);
+  });
 });
