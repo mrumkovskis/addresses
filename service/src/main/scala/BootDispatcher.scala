d0117a5d5ca67fc1c7608b469685646e932cf303
Martins Rumkovskis <martins@uniso.lv>
2020-10-10 12:39:01 +0300

    cleanup

--- service/src/main/scala/BootDispatcher.scala
+++ service/src/main/scala/BootDispatcher.scala
@@ -1,11 +1,10 @@
 package lv.addresses
 
 object BootDispatcher extends scala.App {
-
   if (args.length == 0) {
-    lv.addresses.service.Boot.main(args)
+    service.Boot.main(args)
   } else if (args(0) == "sync") {
-    lv.addresses.Updater.main(args.tail)
+    Updater.main(args.tail)
   } else {
     lv.addresses.service.Boot.main(args)
   }
